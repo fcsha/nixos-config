61ceef996ecb41217ec29646e2f83f9f4c710924
Fucheng Sha <fcsha@qq.com>
2026-06-09 11:10:14 +0800

    Add Home Manager git configuration

--- flake.nix
+++ flake.nix
@@ -3,13 +3,34 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, ... }: {
+  outputs = { nixpkgs, home-manager, ... }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.fc = {
+            home.stateVersion = "26.05";
+
+            programs.git = {
+              enable = true;
+              package = null;
+              settings.user = {
+                name = "Fucheng Sha";
+                email = "fcsha@qq.com";
+              };
+            };
+          };
+        }
       ];
     };
   };
