c00672787e3e72d18724e6bb98b4189999a4a0ec
Fucheng Sha <fcsha@qq.com>
2026-06-09 20:57:33 +0800

    Fix niri corner radius config

--- niri.nix
+++ niri.nix
@@ -43,7 +43,12 @@
 
       window-rules = [
         {
-          geometry-corner-radius = 0;
+          geometry-corner-radius = {
+            top-left = 0.0;
+            top-right = 0.0;
+            bottom-right = 0.0;
+            bottom-left = 0.0;
+          };
           clip-to-geometry = true;
         }
         {
