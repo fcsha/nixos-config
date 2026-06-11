a6e3596893e42eb5cc78779e6c7327fa9345a1d2
Fucheng Sha <fcsha@qq.com>
2026-06-11 15:41:26 +0800

    Add alacritty window padding 12px

--- home.nix
+++ home.nix
@@ -73,6 +73,16 @@
     };
   };
 
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      window.padding = {
+        x = 12;
+        y = 12;
+      };
+    };
+  };
+
   imports = [
     ./niri.nix
   ];
