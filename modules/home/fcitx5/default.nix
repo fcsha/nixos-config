43b75c8809a36a2439aa79b78a173e3d9df7b63a
Fucheng Sha <fcsha@qq.com>
2026-06-12 18:10:13 +0800

    Add fcitx5 custom theme with vertical candidate list

--- modules/home/fcitx5/default.nix
+++ modules/home/fcitx5/default.nix
@@ -2,5 +2,7 @@
 
 {
   xdg.configFile."fcitx5/config".source = ./config;
+  xdg.configFile."fcitx5/conf/classicui.conf".source = ./classicui;
   xdg.dataFile."fcitx5/rime/default.custom.yaml".source = ./rime/default.custom.yaml;
+  xdg.dataFile."fcitx5/themes/fc/theme.conf".source = ./themes/fc/theme.conf;
 }
